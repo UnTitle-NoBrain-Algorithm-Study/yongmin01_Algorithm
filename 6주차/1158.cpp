557513995553d861059a9126a37f3ba7f417d8c3
yongmin01 <heart77123@naver.com>
2022-05-09 08:49:35 +0900

    언타이틀 알고리즘 스터디 6주차 Level 2 요세푸스 문제

---
+++ 6주차/1158.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    int K;
+
+    cin >> N >> K;
+
+    queue<int> arr;
+    
+    for (int i = 1; i <= N; i++)
+    {
+        arr.push(i);
+    }
+
+    cout << "<";
+    while (arr.size() > 1)
+    {
+        for (int i = 0; i < K-1; i++)
+        {
+            arr.push(arr.front());
+            arr.pop();
+        }
+        cout << arr.front() << ", ";
+        arr.pop();
+    }
+
+    cout << arr.front() << ">";
+
+    return 0;
+}
