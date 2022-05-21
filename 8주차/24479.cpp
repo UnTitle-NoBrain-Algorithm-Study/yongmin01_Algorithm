ea69316053db6f6bff3b6a862a942ca4b7d28d65
yongmin01 <heart77123@naver.com>
2022-05-22 04:42:16 +0900

    언타이틀 알고리즘 스터디 8주차 Level 2 알고리즘 수업 - 픽이 우선 탐색 1

--- 8주차/24479.cpp
+++ 8주차/24479.cpp
@@ -9,24 +9,29 @@ int visited[100001] = {0};
 int cnt;
 int result[100001] = {0, };
  
-void dfs(int x){
+void dfs(int x)
+{
     if(visited[x]) return;
     visited[x] = 1;
     cnt++;
     result[x] = cnt;
-    for(int i = 0; i < v[x].size(); i++){
+
+    for(int i = 0; i < v[x].size(); i++)
+    {
         int y = v[x][i];
         dfs(y);
     }
 }
  
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) 
+{
     int N, M, R;
     int a, b;
     
     cin >> N >> M >> R;
     
-    for(int i = 0; i < M; i++){
+    for(int i = 0; i < M; i++)
+    {
         cin >> a >> b;
         v[a].push_back(b);
         v[b].push_back(a);
